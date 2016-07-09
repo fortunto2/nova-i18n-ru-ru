c91d32d54b78918fa5fc81b9efc7750c7cf62f84
fortunto2 <info@3dstr.ru>
2016-07-09 06:24:31 +0500

    first draft

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: "lifefilm:nova-i18n-ru-ru",
+  summary: "Telescope i18n package (ru)",
+  version: "0.26.4-nova",
+  git: "https://github.com/TelescopeJS/telescope.git"
+});
+
+Package.onUse(function (api) {
+
+  api.versionsFrom("METEOR@1.0");
+
+  api.use([
+    'nova:core@0.26.4-nova'
+  ]);
+
+  api.addFiles([
+    'lib/ru_RU.js'
+  ], ["client", "server"]);
+});
